6996dd2a1b7864388b8a21709ae9c629c735d9a4
Victor Assis <victor_assis@poli.ufrj.br>
2024-03-14 18:39:45 -0300

    feat: Wrap Logo in a Link to Home

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,18 +3,19 @@ import Image from 'next/image'
 
 import { NavMenu } from '../navigation/nav-menu'
 import { useScreenSize } from '@/hooks/use-screen-size'
+import Link from 'next/link'
 
 export function Header() {
   const { screenWidth } = useScreenSize()
 
   return (
-    <header className="flex items-center justify-center px-4 py-5">
-      <div className="flex items-center gap-2">
-        <Image src={'logo.svg'} alt="Logo" width={screenWidth >= 768 ? 80 : 36} height={screenWidth >= 768 ? 80 : 36}/>
-        <span className="font-title text-3xl md:text-6xl font-bold">ADAS</span>
-      </div>
+    <header className="flex items-center px-4 py-5 gap-2">
+      <Link href='/'  className="flex items-center gap-2">
+        <Image src={'logo.svg'} alt="Logo" width={screenWidth >= 768 ? 96 : 36} height={screenWidth >= 768 ? 96 : 36}/>
+        <span className="block font-title text-3xl md:text-6xl font-bold border-box">ADAS</span>
+      </Link>
 
-      <div className="flex w-full justify-end">
+      <div className="flex flex-grow justify-end">
         <NavMenu />
       </div>
     </header>
