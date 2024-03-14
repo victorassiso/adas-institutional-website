d975b96cb79d4caa3a8dd890bba2f5b027991b23
Victor Assis <victor_assis@poli.ufrj.br>
2024-03-14 17:24:56 -0300

    feat: Add Footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,7 +1,40 @@
+import { Facebook, Instagram, Linkedin, Twitter } from 'lucide-react'
+import Image from 'next/image'
+
 export function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="text-muted-foreground flex flex-col gap-2 bg-zinc-800 px-10 pb-2 pt-5">
+      <div className="flex gap-4">
+        <div className="flex items-center gap-1.5">
+          <Image src={'logo.svg'} alt="Logo" width={30} height={30} />
+          <span className="font-title text-2xl font-bold">ADAS</span>
+        </div>
+
+        <div className="text-muted-foreground flex w-full justify-center gap-8 text-sm">
+          <div className="flex items-center gap-1">
+            <Twitter size={16} />
+            <span>@ada-saquarema</span>
+          </div>
+
+          <div className="flex items-center gap-1">
+            <Linkedin size={16} />
+            <span>/ada-saquarema</span>
+          </div>
+
+          <div className="flex items-center gap-1">
+            <Facebook size={16} />
+            <span>ada-saquarema</span>
+          </div>
+
+          <div className="flex items-center gap-1">
+            <Instagram size={16} />
+            <span>@ada-saquarema</span>
+          </div>
+        </div>
+      </div>
+      <span className="text-center text-sm">
+        © ADAS | Todos os direitos reservados.
+      </span>
     </div>
   )
 }
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -7,7 +7,7 @@ export function Header() {
     <header className="flex items-center justify-center px-4 py-5">
       <div className="flex items-center gap-2">
         <Image src={'logo.svg'} alt="Logo" width={80} height={80} />
-        <span className="font-title text-6xl font-bold">ADA</span>
+        <span className="font-title text-6xl font-bold">ADAS</span>
       </div>
 
       <div className="flex w-full justify-end">
