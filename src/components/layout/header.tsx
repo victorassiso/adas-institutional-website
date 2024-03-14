4f29d04c5bded29e3a8a65989f13abc55c617bf9
Victor Assis <victor_assis@poli.ufrj.br>
2024-03-14 18:16:56 -0300

    fix: header's logo responsiviness

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,13 +1,17 @@
+'use client'
 import Image from 'next/image'
 
 import { NavMenu } from '../navigation/nav-menu'
+import { useScreenSize } from '@/hooks/use-screen-size'
 
 export function Header() {
+  const { screenWidth } = useScreenSize()
+
   return (
     <header className="flex items-center justify-center px-4 py-5">
       <div className="flex items-center gap-2">
-        <Image src={'logo.svg'} alt="Logo" width={80} height={80} />
-        <span className="font-title text-6xl font-bold">ADAS</span>
+        <Image src={'logo.svg'} alt="Logo" width={screenWidth >= 768 ? 80 : 36} height={screenWidth >= 768 ? 80 : 36}/>
+        <span className="font-title text-3xl md:text-6xl font-bold">ADAS</span>
       </div>
 
       <div className="flex w-full justify-end">
